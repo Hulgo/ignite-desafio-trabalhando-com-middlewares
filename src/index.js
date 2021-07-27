efd2f024c8dee51ca0d7a93fbba25235a8b83d1c
Hulgo Leonardo Jacinto Andrade <hulgoleo@gmail.com>
2021-07-27 14:56:32 -0300

    Desafio Concluído

--- src/index.js
+++ src/index.js
@@ -10,19 +10,70 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const userFound = users.find(user => user.username === username);
+
+  if (!userFound) {
+      return response.status(404).json({ error: "User not found!"});
+  }
+
+  request.user = userFound;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  const { todos, pro} = user;
+
+  if (todos.length > 9 && !pro) {
+    return response.status(403).json({ error: "Maxiumum number of Todos reached. Go a Pro plan."});
+  }
+
+  return next();
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  
+  const { id } = request.params;
+
+  if(!validate(id)) {
+    return response.status(400).json({ error: "ID is not a valid UUID value."})
+  }
+
+  const userFound = users.find(user => user.username === username);
+
+  if (!userFound) {
+    return response.status(404).json({ error: "User not found!"});
+  }
+
+  const todoFound = userFound.todos.find(todo => todo.id === id);
+
+  if (!todoFound) {
+    return response.status(404).json({ error: "Todo not found!"});
+  }
+
+  request.user = userFound;
+  request.todo = todoFound;
+
+  return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const userFound = users.find(user => user.id === id);
+
+  if (!userFound) {
+    return response.status(404).json({ error: "User not found!"});
+  }
+
+  request.user = userFound;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
